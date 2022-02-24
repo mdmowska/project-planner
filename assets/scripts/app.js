5eed40bb1e85b9256d1929970e33fc7dfa6d4714
mdmowska <mk.dmowska@gmail.com>
2022-02-24 23:02:39 +0100

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+import { ProjectList } from './App/ProjectList.js';
+
+globalThis.DEFAULT_VALUE = 'MAX';
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
+
+  }
+
+}
+
+App.init();
